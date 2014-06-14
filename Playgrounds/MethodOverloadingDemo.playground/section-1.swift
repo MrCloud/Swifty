074f70ad2ae0608d4e99a3f5a265284df8815522
Florian PETIT <florian37@me.com>
2014-06-14 16:32:37 +0200

    Added Overloading playground and edited keynote file

---
+++ Playgrounds/MethodOverloadingDemo.playground/section-1.swift
@@ -0,0 +1,19 @@
+// Playground - noun: a place where people can play
+
+func aMethod(param1: String, param2: Int) {}
+func aMethod(param2: Int, param1: String) {}
+func aMethod(p1: String, p2: Int) {} // Error
+func aMethod(param1: String) -> String {
+return ""
+}
+func aMethod(param1: String) -> Int {
+  return 1
+}
+
+
+/* Method overloading :
+      - Method Name
+      - Parameters Type
+      - Return Type
+*/
+    
